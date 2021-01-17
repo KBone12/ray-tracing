bb2a1b466420d3f14dfb02e46035760bbc353ce6
kbone <kbonehobby@gmail.com>
2021-01-17 17:06:38 +0900

    Antialiasing

---
+++ src/camera.rs
@@ -0,0 +1,39 @@
+use cgmath::{Point3, Vector3};
+
+use crate::Ray;
+
+pub struct Camera {
+    origin: Point3<f64>,
+    lower_left_corner: Point3<f64>,
+    horizontal: Vector3<f64>,
+    vertical: Vector3<f64>,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+
+        let origin = Point3::new(0.0, 0.0, 0.0);
+        let horizontal = Vector3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vector3::new(0.0, viewport_height, 0.0);
+        Self {
+            origin,
+            horizontal,
+            vertical,
+            lower_left_corner: origin
+                - horizontal / 2.0
+                - vertical / 2.0
+                - Vector3::new(0.0, 0.0, focal_length),
+        }
+    }
+
+    pub fn ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::io::Write;
 
 use cgmath::{InnerSpace, Point3, Vector3};
+use rand::{distributions::Uniform, prelude::Distribution, rngs::SmallRng, SeedableRng};
 
+mod camera;
 mod hittable;
-use crate::hittable::{Hittable, Sphere};
+use crate::{
+    camera::Camera,
+    hittable::{Hittable, Sphere},
+};
 
 pub struct Ray {
     pub origin: Point3<f64>,
@@ -36,13 +41,16 @@ impl Ray {
 
 type Color = Vector3<f64>;
 
-fn write_color<W: Write>(mut writer: W, color: Color) {
+fn write_color<W: Write>(mut writer: W, color: Color, samples_per_pixel: usize) {
+    let r = color.x / samples_per_pixel as f64;
+    let g = color.y / samples_per_pixel as f64;
+    let b = color.z / samples_per_pixel as f64;
     writeln!(
         writer,
         "{} {} {}",
-        (255.999 * color.x) as i32,
-        (255.999 * color.y) as i32,
-        (255.999 * color.z) as i32
+        (256.0 * r.max(0.0).min(0.999)) as i32,
+        (256.0 * g.max(0.0).min(0.999)) as i32,
+        (256.0 * b.max(0.0).min(0.999)) as i32,
     )
     .expect("Couldn't write a color");
 }
@@ -51,21 +59,19 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: usize = 400;
     const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
+    const SAMPLES_PER_PIXEL: usize = 100;
 
     let hittables = vec![
         Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5),
         Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0),
     ];
 
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0;
+    let camera = Camera::new();
 
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vector3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vector3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vector3::new(0.0, 0.0, focal_length);
+    let distribution = Uniform::from(0.0..1.0);
+    let mut rng = SmallRng::seed_from_u64(
+        0b0101010101010101_0101010101010101_0101010101010101_0101010101010101,
+    );
 
     // Print in PPM Image format
     println!("P3");
@@ -74,14 +80,14 @@ fn main() {
     for y in 0..IMAGE_HEIGHT {
         eprintln!("Scan lines remaining: {}", IMAGE_HEIGHT - y);
         for x in 0..IMAGE_WIDTH {
-            let u = (x as f64) / (IMAGE_WIDTH as f64 - 1.0);
-            let v = ((IMAGE_HEIGHT - y) as f64) / (IMAGE_HEIGHT as f64 - 1.0);
-            let ray = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
-            let color = ray.color(&hittables);
-            write_color(std::io::stdout(), color);
+            let color = (0..SAMPLES_PER_PIXEL).fold(Color::new(0.0, 0.0, 0.0), |acc, _| {
+                let u = (x as f64 + distribution.sample(&mut rng)) / (IMAGE_WIDTH as f64 - 1.0);
+                let v = ((IMAGE_HEIGHT - y) as f64 + distribution.sample(&mut rng))
+                    / (IMAGE_HEIGHT as f64 - 1.0);
+                let ray = camera.ray(u, v);
+                acc + ray.color(&hittables)
+            });
+            write_color(std::io::stdout(), color, SAMPLES_PER_PIXEL);
         }
     }
     eprintln!("Done");
